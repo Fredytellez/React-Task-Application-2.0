a051c7ecf680c20c8cf88795d02f51a52a4faca6
Fredy Pena <tredy@outlook.com>
2023-10-13 11:19:34 -0500

    Quinto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import TaskList from "./TaskList";
 import TaskForm from "./TaskForm";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task){
+    setTasks([...tasks, task])
+  }
+
   return (
     <>
-      <TaskForm />
-      <TaskList />
+      <TaskForm createTask={createTask}/>
+      <TaskList tasks={tasks} />
     </>
   );
 }
--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,9 +1,28 @@
-function TaskForm() {
+import { useState } from "react";
+
+function TaskForm({ createTask }) {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      title,
+      id: 4,
+      description: "Nueva tarea",
+    };
+    createTask(newTask);
+  };
+
   return (
-    <form>
-        <input placeholder="Escribe tu tarea"/>
-        <button>Guardar</button>
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Escribe tu tarea"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <button>Guardar</button>
     </form>
-  )
+  );
 }
-export default TaskForm
+export default TaskForm;
